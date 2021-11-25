b7e9112d53156e4fa8dfbee853da088c512e3304
feds9anta <vincentelli.fm@gmail.com>
2021-11-25 09:24:40 +0000

    Update lag_prediction.py

--- lag_prediction.py
+++ lag_prediction.py
@@ -1,9 +1,9 @@
 import numpy as np
 '''
-Code to print expected lag spectrum from equation 8 of Kammoun+2021 ApH 907, Issue 1, id.20 
+Code to evaluate the expected lag spectrum for an AGN, given its physical parameters.
+Based on equation 8 of Kammoun+2021 ApH 907, Issue 1, id.20 
 ( https://ui.adsabs.harvard.edu/abs/2021ApJ...907...20K/abstract )
  
-
 Parameters defined in the code are for Mrk 110
 
 F. Vincentelli 2021
@@ -14,14 +14,18 @@ F. Vincentelli 2021
 # define parameters
 h=5#h is the height of the lamp-post in units of 10 Rg
 mdot =8#is the accretion rate in units of 5% of the Eddington limit, 
-LX=0.027#is the observed, 2−10 keV luminosity in units of 0.01 of the Eddington luminosity,
+LX=0.027#is the observed, 2−10 keV luminosity in units of the Eddington luminosity,
 M= 2#M7 is the BH mass in units of 107M
-lambda_ref=1928 # UVW2 REFERENCE BAND
-lambda0=1950 # NORMALIZATION
+lambda_ref=1928 # UVW2 REFERENCE BAND  (Angstrom)
+lambda0=1950 # NORMALIZATION, fixed  (Angstrom)
 spin =1
 
-wave_inf =0 #lower limit of lag spectrum 
-wave_up=10000 #upper limit of lag spectrum 
+lag_choice=0 #0 if a single wavelength, 1 if all lag spectrum
+
+lamb_single=0.25 #lambda in Angstrom for which the single lag is computed
+
+wave_inf =0 #lower limit of lag spectrum (Angstrom)
+wave_up=10000 #upper limit of lag spectrum  (Angstrom)
 wave=np.linspace(wave_inf,wave_up,wave_up*4)
 
 if spin==0:
@@ -41,12 +45,10 @@ if spin==1:
 
 	#choice=input("Spin 0 or 1 ?")
 
-lamb=0.25
-print(A* (M**0.7 )* f1 *f2 * ((lamb/lambda0) **B - (lambda_ref/lambda0)**B ))
-
-
-
-#for i in range(0,len(wave)) :
-#	lamb=wave[i]
-#	print(lamb, A* (M**0.7 )* f1 *f2 * ((lamb/lambda0) **B - (lambda_ref/lambda0)**B ))
+if lag_choice==0:
+	print(A* (M**0.7 )* f1 *f2 * ((lamb/lambda0) **B - (lambda_ref/lambda0)**B ))
+if lag_choice==1:
+	for i in range(0,len(wave)) :
+		lamb=wave[i]
+		print(lamb, A* (M**0.7 )* f1 *f2 * ((lamb/lambda0) **B - (lambda_ref/lambda0)**B ))
  
